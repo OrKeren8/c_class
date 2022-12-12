cc34a5fa140aae29f84b5c7aa5fbe18c5b9aa2a6
orkeren <orkeren8@gmail.com>
2022-12-12 23:04:16 +0200

    get semester courses data

---
+++ class7/HW_targil5.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define MAX_COURSES 10
+#define SEMESTER_A 'A'
+#define SEMESTER_B 'B'
+
+struct CourseInfo
+{
+    int courseNum;
+    int grade;
+} typedef COURSE_INFO;
+
+void welcomeCall();
+void getSemesterCourses(char semester, COURSE_INFO courseArr[]);
+
+void main()
+{
+    COURSE_INFO firstSemesterCoursesArr[MAX_COURSES];
+    COURSE_INFO secondSemesterCoursesArr[MAX_COURSES];
+    welcomeCall();
+    getSemesterCourses(SEMESTER_A, firstSemesterCoursesArr);
+    getSemesterCourses(SEMESTER_B, secondSemesterCoursesArr);
+}
+
+void welcomeCall()
+{
+    /*introduction to the student
+
+    Args: none
+    return: none
+    */
+    printf("Welcome students!!\n"
+           "and bye bye Pizzeria\n");
+}
+
+void getSemesterCourses(char semester, COURSE_INFO courseArr[])
+{
+    /*get courses data from the user of a specific semester
+
+    Args:
+        char semester: the name of the semester
+        COURSE_INFO courseArr[]: array of course info structure
+    return: none
+    */
+    int numOfCourses, courseNumber, grade;
+    printf("Please enter number of courses in semester %c: ", semester);
+    scanf("%d", &numOfCourses);
+    for (int i = 0; i < numOfCourses; i++)
+    {
+        printf("Enter course number and grade: ");
+        scanf("%d %d", &courseArr[i].courseNum, &courseArr[i].grade);
+    }
+}
